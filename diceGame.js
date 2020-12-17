b27136bbe138f0daf7fec5292ef11ddc135c13b8
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2020-12-17 10:57:16 +0000

    new dice files

---
+++ diceGame.js
@@ -0,0 +1,28 @@
+const score = document.getElementById("score");
+const dice = document.getElementById("dice");
+const button = document.getElementById("roll");
+const player = document.getElementById("player");
+
+button.addEventListener("click", () => {
+    if (button.innerHTML == "Play Again?") {
+        button.innerHTML = "roll";
+        player.innerHTML = "Player 1";
+        score.innerHTML = 0;
+    }
+    let x = Math.floor(Math.random() * 6) + 1;
+    let y = parseInt(score.innerHTML);
+    dice.src = `./Dice ${x}.png`;
+    
+    if (x === 1) {
+        player.innerHTML = "You Lose!";
+        button.innerHTML = "Play Again?";
+    }
+    else if (score.innerHTML < 20) {
+        y += x;
+        score.innerHTML = y
+    }
+    else if (y >= 20) {
+        player.innerHTML = "You Win!";
+        button.innerHTML = "Play Again?";
+    }
+}) 
